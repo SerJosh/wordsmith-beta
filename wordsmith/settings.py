d0976bf3c9db4a8f8e238f7692e5c9dcbe23605b
SerJosh <joshronan25@gmail.com>
2024-02-20 12:03:39 +0000

    admin stuff

--- wordsmith/settings.py
+++ wordsmith/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.8000-serjosh-wordsmithbeta-7ve5nxyp682.ws-eu108.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
