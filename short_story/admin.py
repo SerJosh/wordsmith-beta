ad78d671b505fcd0dc850538619f5085b9665e1b
SerJosh <joshronan25@gmail.com>
2024-02-22 12:44:40 +0000

    complete admin module

--- short_story/admin.py
+++ short_story/admin.py
@@ -6,8 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Story)
 class StoryAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug',)
-    search_fields = ['title']
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
