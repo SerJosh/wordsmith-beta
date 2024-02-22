55b7afbd603f3f473935e5a1b5a530659298ae81
SerJosh <joshronan25@gmail.com>
2024-02-22 10:45:32 +0000

    unsuccessfully add admin filter

--- short_story/admin.py
+++ short_story/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Story
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Story)
+class StoryAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug',)
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Story)
